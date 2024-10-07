1fb20ff5ab8816a71418fa6ac9517c83da91fe96
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-07 16:07:35 +0100

    sprint

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,16 +1,30 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import customFetch from "./util";
+import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
-  const { mutate: createTask, isLoading } = useMutation({
-    mutationFn: () => customFetch.post("/", { title: "greatness is here" }),
+  const queryClient = useQueryClient();
+  const {
+    mutate: createTask,
+    isLoading,
+    tasks,
+  } = useMutation({
+    mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [tasks] });
+      toast.success("task added");
+      setNewItemName("");
+    },
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    createTask();
+    createTask(newItemName);
   };
   return (
     <form onSubmit={handleSubmit}>
